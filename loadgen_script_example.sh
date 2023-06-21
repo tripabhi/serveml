53ae5c896aa02b9a168e6118a95bfb277cd0bd35
Abhijit Tripathy <abhijittripathy.projects@gmail.com>
2023-06-21 19:49:13 +0000

    Updated conf for Locust

--- loadgen_script_example.sh
+++ loadgen_script_example.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-ab -c 1000 -n 15000 -p ./request_body_example.json -T application/json http://10.152.183.147/predict
+ab -c 1000 -n 15000 -e exp1_batch.csv -p ./request_body_example.json -T application/json http://bert.traefik.local/predict
