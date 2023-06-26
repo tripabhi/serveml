33456bfbefa5197a0c7ebd10ee05f96a505e0947
Chameleon Cloud User <cc@knode-1.novalocal>
2023-06-26 00:48:06 +0000

    Change endpoint for benchmarking torchserve

--- loadgen/locustfile.py
+++ loadgen/locustfile.py
@@ -7,7 +7,7 @@ class InferenceTestUser(FastHttpUser):
     @task
     def predict(self):
         self.client.post(
-            "/predictions/bert_sa",
+            "/predictions/benchmark",
             json={
                 "text": "Bloomberg has decided to publish a new report on the global economy.",
                 "target": 1,
